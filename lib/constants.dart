8adef7650f2358580526c208bdd142fd27ed6f59
Dulakshi RB <dulakshirb@gmail.com>
2022-11-16 08:46:16 +0530

    UI and calculate VMI

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFF306eb3);
+const kOutputTextColor = Color(0xFF0D4C92);
+const kInputLabelColor = TextStyle(
+  color: kTextColor,
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
---
+++ lib/main_page.dart
@@ -0,0 +1,186 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int height = 150;
+  int weight = 50;
+
+  late double bmi = calculateBMI(height: height, weight: weight);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: const Color(0xFFCFF5E7),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 25),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    color: Colors.blue.withAlpha(50),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: const [
+                        Icon(
+                          Icons.male,
+                          size: 100,
+                          color: Color(0xFF59C1BD),
+                        ),
+                        Text("Male"),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: const [
+                        Icon(
+                          Icons.female,
+                          size: 100,
+                          color: Color(0xFFF7A4A4),
+                        ),
+                        Text("Female"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        const Text("Height"),
+                        Text("$height", style: kInputLabelColor),
+                        Row(
+                          children: [
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  if (height > 50) height--;
+                                  bmi = calculateBMI(
+                                      height: height, weight: weight);
+                                });
+                              },
+                              child: Icon(
+                                Icons.remove,
+                                size: 50,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 25,
+                            ),
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  if (height < 220) height++;
+                                  bmi = calculateBMI(
+                                      height: height, weight: weight);
+                                });
+                              },
+                              child: Icon(
+                                Icons.add,
+                                size: 50,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        const Text("Weight"),
+                        Text("$weight", style: kInputLabelColor),
+                        Row(
+                          children: [
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  if (weight > 32) weight--;
+                                  bmi = calculateBMI(
+                                      height: height, weight: weight);
+                                });
+                              },
+                              child: Icon(
+                                Icons.remove,
+                                size: 50,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 25,
+                            ),
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  if (weight < 350) weight++;
+                                  bmi = calculateBMI(
+                                      height: height, weight: weight);
+                                });
+                              },
+                              child: Icon(
+                                Icons.add,
+                                size: 50,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Column(
+                children: [
+                  const Text("BMI"),
+                  Text(
+                    "${bmi.toStringAsFixed(2)}",
+                    style: kInputLabelColor.copyWith(
+                        color: kOutputTextColor, fontSize: 70),
+                  ),
+                  Text(getResult(bmi))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  double calculateBMI({required int height, required int weight}) {
+    return (weight / (height * height)) * 10000;
+  }
+
+  static String getResult(bmiValue) {
+    if (bmiValue >= 40) {
+      return 'Obese';
+    } else if (bmiValue >= 25) {
+      return 'Overweight';
+    } else if (bmiValue > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+}
